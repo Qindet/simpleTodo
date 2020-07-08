16d253c539f631fc10aa3ab6855676ce1ff93b67
Qindet <flgre890@mail.ru>
2020-07-08 16:49:05 +0500

    state

--- src/index.js
+++ src/index.js
@@ -1,25 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import AppHeader from "./components/app-header"
-import SearchPanel from "./components/search-panel"
-import TodoList from "./components/todo-list"
-
-const App = () => {
-
-    const todoData = [
-        {label: 'Drink', important: false, id:1},
-        {label: 'Fight', important: true, id:2},
-        {label: 'Work', important: true, id:3}
-    ]
-
-    return (
-        <div>
-            <AppHeader/>
-            <SearchPanel/>
-            <TodoList todos={todoData}/>
-        </div>
-    )
-}
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from "./components/app";
 
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />,
+  document.getElementById('root'));
